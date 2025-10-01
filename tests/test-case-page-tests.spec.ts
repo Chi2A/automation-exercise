4db7b8af28a2f07d2a19a37ba3a223c5cad89905
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-30 21:07:34 -0500

    commit

--- tests/test-case-page-tests.spec.ts
+++ tests/test-case-page-tests.spec.ts
@@ -33,6 +33,7 @@ test.describe("Search Page Tests", () => {
     test('Cart Functionality', async ({ page }) => {
         await homePage.clickOnNavLink("Cart");
         await basePage.verifyFooterTextIsVisible();
+        await basePage.enterEmailAddress("test@example.com");
         await basePage.verifySuccessMessage();
     })
 });
