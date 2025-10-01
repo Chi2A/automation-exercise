f06c2467488327148b374d390b3e0c044e3c1b28
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-30 21:36:55 -0500

    a

--- pages/base-page.ts
+++ pages/base-page.ts
@@ -3,36 +3,38 @@ import { threadCpuUsage } from "process";
 
 export class BasePage {
   page: Page;
-    private topNavigationLocators: Locator;
-    private footerText: Locator;
-   private emailAddressField: Locator;
-    private arrowButton: Locator;
-    private successMessage: Locator;
+  private topNavigationLocators: Locator;
+  private footerText: Locator;
+  private emailAddressField: Locator;
+  private arrowButton: Locator;
+  private successMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.topNavigationLocators = page.locator(`ul[class="nav navbar-nav"] li`); // all nav locators
-      this.footerText = page.getByRole("heading", { name: "Subscription" });
+    this.footerText = page.getByRole("heading", { name: "Subscription" });
     this.emailAddressField = page.getByRole("textbox", {
       name: "Your email address",
     });
     this.arrowButton = page.getByRole("button", { name: "" });
-    this.successMessage = page.locator('div[class="alert-success alert"]')
+    this.successMessage = page.locator('div[class="alert-success alert"]');
   }
 
   async clickOnNavLink(linkText: string) {
     await this.topNavigationLocators.getByText(linkText).click();
   }
   async verifyFooterTextIsVisible(): Promise<void> {
-      await expect(this.footerText).toBeVisible();
-      await expect(this.footerText).toHaveText('Subscription');
+    await expect(this.footerText).toBeVisible();
+    await expect(this.footerText).toHaveText("Subscription");
   }
-    async enterEmailAddress(email: string): Promise<void> {
-        await this.emailAddressField.fill(email);
-        await this.arrowButton.click();
-    }
+  async enterEmailAddress(email: string): Promise<void> {
+    await this.emailAddressField.fill(email);
+    await this.arrowButton.click();
+  }
   async verifySuccessMessage(): Promise<void> {
     await expect(this.successMessage).toBeVisible();
-    await expect(this.successMessage).toHaveText("You have been successfully subscribed!");
+    await expect(this.successMessage).toHaveText(
+      "You have been successfully subscribed!"
+    );
   }
 }
