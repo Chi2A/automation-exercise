90a431428f581271bb82c09bccfe60ee1369dad6
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-10-01 21:11:49 -0500

    formatted

--- pages/brands-page.ts
+++ pages/brands-page.ts
@@ -8,7 +8,7 @@ export class BrandsPage extends BasePage {
     super(page);
     this.brandsProductsTitle = page.locator('h2[class="title text-center"]');
   }
-    async verifyBrandsProductsTitle(expectedTitle:string): Promise<void> {
+  async verifyBrandsProductsTitle(expectedTitle: string): Promise<void> {
     await expect(this.brandsProductsTitle).toBeVisible();
     await expect(this.brandsProductsTitle).toHaveText(expectedTitle);
   }
